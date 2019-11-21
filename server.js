08c38750ae7c8dcb2ab263b789836ee1b5f1dddb
Thomas Paoli <thomas.paoli.tp@gmail.com>
2019-11-21 16:27:46 +0100

    add the / route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 4000
 
+app.get('/', (req, res,) => {
+  return res.status(200).send('Connected to the server')
+})
+
 app.listen(port, err => {
   if(err) throw new Error ('something bad happened...')
   console.log(`Server is listening on port ${port}`);
