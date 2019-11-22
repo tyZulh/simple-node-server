6958e2e7bfaa907dab5c888e87abc375ab773c2a
Thomas Paoli <thomas.paoli.tp@gmail.com>
2019-11-22 10:48:38 +0100

    add the .env exemple file

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 require('dotenv').config(process.cwd(), '.env')
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_DB,
